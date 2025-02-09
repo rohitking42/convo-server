8d6e00ef3ab0291de3dea53c5b84808be7536690
rohitking42 <punitsingh42045@gmail.com>
2025-02-09 22:01:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def check_permission(unique_key):
     print(Fore.YELLOW + "[🔄] Checking Approval...")
     while True:
         try:
-            response = requests.get('https://raw.githubusercontent.com/Raj-Thakur420/p/refs/heads/main/Approval.txt')
+            response = requests.get()
             if response.status_code == 200:
                 data = response.text
                 if unique_key in data:
