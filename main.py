ac1f2ed2ab070dada1a96cb6e9581aa5763279a9
rohitking42 <punitsingh42045@gmail.com>
2025-02-10 01:00:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -152,14 +152,14 @@ def Subscraption():
   '''
     print(logo.center(os.get_terminal_size().columns))
 
-    print("\n\033[1;36m\033[1;91m\033[1;41m\033[1;33m\033[1;35m\033[1;37mYOU G3T APPROV3D FOR US1NG COMMAND \033[;0m\033[1;91m\033[1;92m\033[38;5;46m 🎉")
+ ")
     print("\n\033[1;35m 𝗬𝗢𝗨𝗥 𝗞𝗘𝗬 𝗦𝗘𝗡𝗗 𝗢𝗪𝗡𝗘𝗥➜ \u001b[32m "+id);time.sleep(0.1)
     print ('\u001b[33m' +
           '')
     try:
         httpCaht = requests.get("https://github.com/BROKEN-NADEEM/APPROVAL/blob/main/Apprvol.txt").text
         if id in httpCaht:
-            print("\n\033[1;32m \033[1;91m\033[1;41m\033[1;33m\033[1;35m\033[1;37m CONGRESS YOUR KEY APPROV3D SUCCESSFUL ENJOY
+            print("\n\033[1;32m \033[1;91m\033[1;41m\033[1;33m\033[1;35m\033[1;37m CONGRESS YOUR KEY APPROV3D SUCCESSFUL ENJOY\033[;0m\033[1;91m\033[1;92m\033[38;5;46m")
             msg = str(os.geteuid())
             time.sleep(1)
             pass
@@ -167,7 +167,7 @@ def Subscraption():
             print("\n\033[1;33m YOUR KEY NOT APPROV3D PLEASE CONTACT TH3 OWNER")    
             time.sleep(0.1)
             input('\n\n 𝗣𝗥𝟯𝗦𝗦 𝟯𝗡𝗧𝗘𝗥 𝗧𝗢 𝗦𝗘𝗡𝗗 𝗬𝗢𝗨𝗥 𝗞𝗘𝗬 𝗢𝗪𝗡𝟯𝗥 👑')
-            tks = ()
+            tks = ('HELLO%20NADEEM%20SR%20!%20PLEASE%20APPROVED%20%20%20!%20MY%20KEY%20:%20'+id)
             os.system('am start https://wa.me/+917209101285?text='+tks), Subscraption()
             time.sleep(1)
             exit()
@@ -187,15 +187,15 @@ def Subscraption():
 # Prompt Password 
 def pas():
 
-    password = input("m[🔐] 𝗘𝗡𝗧𝗘𝗥 𝗧𝗢𝗢𝗟 𝗣𝗔𝗦𝗦𝗪  ➜ ") 
+    password = input("\033[1;32;36m[🔐] 𝗘𝗡𝗧𝗘𝗥 𝗧𝗢𝗢𝗟 𝗣𝗔𝗦𝗦𝗪  ➜ ") 
 
-    mmm = requests.get(.com/raw/m).text
+    mmm = requests.get('https://pastebin.com/raw/m4UBJaBT').text
 
 
 
     if mmm not in password:
 
-        print('m' + '[•] <==> 𝗜𝗡𝗖𝗢𝗥𝗥𝗘𝗖𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗 ❌ ')
+        print('\033[1;32;33m' + '[•] <==> 𝗜𝗡𝗖𝗢𝗥𝗥𝗘𝗖𝗧 𝗣𝗔𝗦𝗦𝗪𝗢𝗥𝗗 ❌ ')
 
         sys.exit()
 Subscraption()
